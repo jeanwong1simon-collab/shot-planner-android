19a831dbf88d0764075c87024fcd4699dc0de7ec
jeanwong1simon-collab <jeanwong1simon@gmail.com>
2026-09-16 12:35:07 +0800

    Add app state assembly

---
+++ app/src/main/assets/data_build.js
@@ -0,0 +1,3 @@
+window.STORAGE_KEY='shot_planner_app_v112';
+const pm=window.APP_PARTS.projectMeta;
+window.PRELOAD_STATE={version:window.APP_PARTS.version,settings:window.APP_PARTS.settings,storyboards:window.APP_PARTS.storyboards,templates:window.APP_PARTS.templates,projects:[Object.assign({},pm,{locations:[window.APP_PARTS.locWujiang,window.APP_PARTS.locFurong]})]};
